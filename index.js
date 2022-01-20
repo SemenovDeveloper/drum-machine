9f20fa2454c890aab24db1312522b98e5897749e
Alexander Semenov <SemenovDev@yahoo.com>
2022-01-20 15:53:06 +0600

    play and clear funct

--- index.js
+++ index.js
@@ -57,20 +57,44 @@ const audioClips = [
 
 function App() { 
   const [volume, setVolume] = React.useState(0.5);
+  const [record, setRecord] = React.useState("");
+  
+
   const changeVolume = (e) => {
     setVolume(e.target.value);
   }
-  let audioId="track";
+
+  const playRecord = () =>{
+    const sounds = record.split(" ");
+    for(let i=0; i < sounds.length; i++){
+      setTimeout( () => {
+        const audioTag = document.getElementById(sounds[i]);
+        console.log(audioTag);
+        audioTag.volume = volume;
+        audioTag.currentTime = 0;
+        audioTag.play();
+      }, i * 500, i);
+    }
+  }
+
+  const clearRecord = () => {
+    setRecord("");
+  }
 
   return (
     <div className="bg-info text-white min-vh-100">
       <div className="text-center">
         <h1>Drum Machine</h1>
         {audioClips.map(clip => (
-          <Pad key={clip.id} clip={clip} volume={volume} audioId={audioId}/>
+          <Pad 
+            key={clip.id} 
+            clip={clip} 
+            volume={volume} 
+            record={record} 
+            setRecord={setRecord}/>
         ))}
         <br/>
-        <div id="display">{audioId}</div>
+        <div id="display"></div>
         <br/>
         <div>
           <h3>Volume</h3>
@@ -85,9 +109,10 @@ function App() {
           <div>{Math.round(volume*100) + "%"}</div>
           <div>
             <div className="btn bg-white m-2">rec</div>
-            <div className="btn bg-white m-2">play</div>
-            <div className="btn bg-white m-2">clear</div>
+            <div className="btn bg-white m-2" onClick={playRecord}>play</div>
+            <div className="btn bg-white m-2" onClick={clearRecord}>clear</div>
           </div>
+          <div>{record}</div>
         </div>
       </div>
     </div>
@@ -119,13 +144,14 @@ function Pad(props) {
       audioTag.currentTime = 0;
       audioTag.play();
       displayClipId();
+      props.setRecord(record => record + props.clip.keyTrigger + " ");
       ;
     }
 
 
     return (
       <div onClick={playSound} className="btn bg-light p-4 m-4">
-        <audio
+        <audio      
           className="clip"
           id={props.clip.keyTrigger}
           src={props.clip.url}
