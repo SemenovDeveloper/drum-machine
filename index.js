bc328dfee43207463c75c15b9d9f27dabe144cc6
Alexander Semenov <SemenovDev@yahoo.com>
2022-01-24 13:07:48 +0600

    some functional

--- index.js
+++ index.js
@@ -2,70 +2,80 @@ const audioClips = [
   {
     keyCode: 81,
     keyTrigger: 'Q',
-    id: 'Chord-1',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
+    id: 'Heater-1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
   },
   {
     keyCode: 87,
     keyTrigger: 'W',
-    id: 'Chord-2',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
+    id: 'Heater-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
   },
   {
     keyCode: 69,
     keyTrigger: 'E',
-    id: 'Chord-3',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
+    id: 'Heater-3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
   },
   {
     keyCode: 65,
     keyTrigger: 'A',
-    id: 'Shaker',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
+    id: 'Heater-4',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
   },
   {
     keyCode: 83,
     keyTrigger: 'S',
-    id: 'Open-HH',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
+    id: 'Clap',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
   },
   {
     keyCode: 68,
     keyTrigger: 'D',
-    id: 'Closed-HH',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
+    id: 'Open-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
   },
   {
     keyCode: 90,
     keyTrigger: 'Z',
-    id: 'Punchy-Kick',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
+    id: "Kick-n'-Hat",
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
   },
   {
     keyCode: 88,
     keyTrigger: 'X',
-    id: 'Side-Stick',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'
+    id: 'Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
   },
   {
     keyCode: 67,
     keyTrigger: 'C',
-    id: 'Snare',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
+    id: 'Closed-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
   }
 ];
+
+const defaultStyle = {
+  backgroundColor: 'white',
+  boxShadow: '3px 3px 5px black'
+};
+
+const activeStyle = {
+  backgroundColor: 'orange',
+  boxShadow: '0 3px orange',
+};
 
 function App() { 
   const [volume, setVolume] = React.useState(0.5);
   const [record, setRecord] = React.useState("");
-  
+  const [recordStatus, setRecordStatus] = React.useState(false);  
 
   const changeVolume = (e) => {
     setVolume(e.target.value);
   }
 
   const playRecord = () =>{
-    const sounds = record.split(" ");
+    const sounds = record.split("");
     for(let i=0; i < sounds.length; i++){
       setTimeout( () => {
         const audioTag = document.getElementById(sounds[i]);
@@ -73,7 +83,7 @@ function App() {
         audioTag.volume = volume;
         audioTag.currentTime = 0;
         audioTag.play();
-      }, i * 500, i);
+      }, i * 350);
     }
   }
 
@@ -81,12 +91,17 @@ function App() {
     setRecord("");
   }
 
+  const recordToggler = () => {
+    setRecordStatus(!recordStatus);
+  }
+
   return (
     <div className="bg-info text-white min-vh-100">
       <div className="text-center">
         <h1>Drum Machine</h1>
         {audioClips.map(clip => (
-          <Pad 
+          <Pad            
+            recordStatus={recordStatus}
             key={clip.id} 
             clip={clip} 
             volume={volume} 
@@ -104,13 +119,13 @@ function App() {
             step="0.01"
             type="range" 
             value={volume}
-            onChange={changeVolume}          
+            onChange={changeVolume}
           ></input>
           <div>{Math.round(volume*100) + "%"}</div>
           <div>
-            <div className="btn bg-white m-2">rec</div>
-            <div className="btn bg-white m-2" onClick={playRecord}>play</div>
-            <div className="btn bg-white m-2" onClick={clearRecord}>clear</div>
+            <div className="btn bg-white m-2" onClick={recordToggler}><i className="fas fa-record-vinyl"></i></div>
+            <div className="btn bg-white m-2" onClick={playRecord}><i className="white fas fa-play"></i></div>
+            <div className="btn bg-white m-2" onClick={clearRecord}><i className="far fa-trash-alt"></i></div>
           </div>
           <div>{record}</div>
         </div>
@@ -120,6 +135,8 @@ function App() {
 }
 
 function Pad(props) {
+    const [padStyle, setPadStyle] = React.useState(defaultStyle)
+
     React.useEffect( () => {
       document.addEventListener("keydown", playByKeyPress);
       return () => {
@@ -132,7 +149,7 @@ function Pad(props) {
       display.innerHTML = props.clip.id;
     }
 
-    const playByKeyPress = (e) => {
+        const playByKeyPress = (e) => {
       if(e.keyCode === props.clip.keyCode) {
         playSound();
       }
@@ -144,13 +161,17 @@ function Pad(props) {
       audioTag.currentTime = 0;
       audioTag.play();
       displayClipId();
-      props.setRecord(record => record + props.clip.keyTrigger + " ");
-      ;
+      setPadStyle(activeStyle);
+      setTimeout( () => {setPadStyle(defaultStyle)},200)
+      if (props.recordStatus) {
+        props.setRecord(record => record + props.clip.keyTrigger);
+      }
     }
 
 
     return (
-      <div onClick={playSound} className="btn bg-light p-4 m-4">
+      <div onClick={playSound} className="btn p-4 m-4" style={padStyle}
+      >
         <audio      
           className="clip"
           id={props.clip.keyTrigger}
